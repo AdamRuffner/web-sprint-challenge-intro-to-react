9ea43ae44e99fab699b785d703e1235c2b2d4b53
Adam <ruffneradam@aol.com>
2020-09-11 13:08:38 -0400

    succesfull pulling of info, working on styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,62 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const CharacterDiv = styled.div`
+    border: 1px solid black;
+    width: 50%;
+    margin: 3% auto;
+    border-radius: 100px;
+    
+ `
+
+const Name = styled.h2`
+    color: white;
+    `
+
+
+
+const Gender = styled.p`
+    
+`
+
+const Height = styled.p`
+
+`
+
+const Mass = styled.p`
+
+`
+
+const BirthYear = styled.p`
+
+`
+
+const EyeColor = styled.p`
+
+`
+
+const HairColor = styled.p`
+
+`
+
+const SkinColor = styled.p`
+
+`
+
+
+
+export default function Character(props){
+    return (
+        <CharacterDiv>
+            <Name>{props.name}</Name>
+            <Gender>Gender: {props.gender}</Gender>
+            <Height>Height {props.height}</Height>
+            <Mass>Mass: {props.mass}</Mass>
+            <BirthYear> Birth Year: {props.birthYear}</BirthYear>
+            <EyeColor>Eye Color: {props.eyeColor}</EyeColor>
+            <HairColor>Hair Color: {props.hairColor}</HairColor>
+            <SkinColor>Skin Color: {props.skinColor}</SkinColor>
+        </CharacterDiv>
+    )
+}
